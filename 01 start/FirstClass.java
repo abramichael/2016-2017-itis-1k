d68a59a665235f032660797667b1f8ccb1800ff7
ma <ma@fosslabs.ru>
2016-09-08 14:01:49 +0300

    Errors in starting template fixed

--- 01 start/FirstClass.java
+++ 01 start/FirstClass.java
@@ -9,11 +9,11 @@ public class FirstClass {
 	public static void main(String [] args) {
 	
 		// Типы данных - целый int, вещественный double 
-		int x = Integer.parseInt(args[0])
+		int x = Integer.parseInt(args[0]);
 		int y = Double.parseInt(args[1]);
 		
 		// арифметика: +, -, *, /, %. При этом на целых числах - целочисленное деление
-		double z + x = y;
+		double z = x + y;
 		
 		// При выводе любой тип конвертируется в строку (String)
 		System.out.println(z);
